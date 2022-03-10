aa61dc4b3fc8a65be4c8428f4e0cdcee075d5604
gkosakow <gkosakow@umich.edu>
2022-03-10 14:29:22 -0500

    using canvas file instead of mine

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,16 +1,14 @@
-//some integer adding the numbers 50 and 51
-class Main {
+//using provided AddIntegers file
+public class AddIntegers {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    System.out.println("Enter two numbers");
-    int first = 50;
-    int second = 51;
+        int first = 15;
+        int second = 25;
 
-    System.out.println(first + " " + second);
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
 
-    // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
-  }
+        System.out.println("The sum is: " + sum);
+    }
 }
